08a2a923d7360077ee923236d3761d96b5575f9c
riccdatt <rdattena@yahoo.com>
2026-06-16 16:57:35 +0200

    Code cleanup

--- app/app/src/main/java/com/example/letmecook_lab5/auth/AuthRepository.kt
+++ app/app/src/main/java/com/example/letmecook_lab5/auth/AuthRepository.kt
@@ -8,60 +8,6 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import com.google.firebase.auth.FirebaseUser
 
-/*
-
-interface AuthRepository {
-    val currentUserId: String?
-    val currentUserStateFlow: StateFlow<String?>
-
-    val isLoggedIn: Boolean
-
-    suspend fun logOut()
-    suspend fun signInAnonymouse(): Result<Unit>
-    suspend fun signIn(context: Context): Result<Unit>
-}
-
-class FirebaseAuthRepository (
-    private val auth: FirebaseAuth,
-    private val credentialManager: CredentialManager
-): AuthRepository {
-
-    override val currentUserId: String?
-        get() = FirebaseAuth.getInstance().currentUser?.uid ?:
-        throw IllegalStateException("User not logged in")
-
-    val _currentUserMutableStateFlow: MutableStateFlow<String?> = MutableStateFlow(FirebaseAuth.getInstance().currentUser?.uid)
-    override val currentUserStateFlow: StateFlow<String?> = _currentUserMutableStateFlow.asStateFlow()
-
-    init {
-        FirebaseAuth.getInstance().addAuthStateListener { auth ->
-            _currentUserMutableStateFlow.value = auth.currentUser?.uid
-        }
-    }
-
-    //TODO()
-    override suspend fun logOut() {
-        //TODO("Not yet implemented")
-    }
-
-    override suspend fun signInAnonymouse(): Result<Unit> {
-        //TODO("Not yet implemented")
-        return Result.success(Unit)
-    }
-
-    override suspend fun signIn(context: Context): Result<Unit> {
-        //TODO("Not yet implemented")
-        return Result.success(Unit)
-    }
-
-    override val isLoggedIn: Boolean
-        get() = FirebaseAuth.getInstance().currentUser != null
-}
-*/
-
-
-
-
 
 interface AuthRepository {
 
--- app/app/src/main/java/com/example/letmecook_lab5/auth/SessionManagerFacade.kt
+++ app/app/src/main/java/com/example/letmecook_lab5/auth/SessionManagerFacade.kt
@@ -15,62 +15,6 @@ import kotlinx.coroutines.tasks.await
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 
-/*
-object SessionManagerFacade: AuthRepository {
-
-    override val currentUserId: String?
-        get() = FirebaseAuth.getInstance().currentUser?.uid ?:
-            throw IllegalStateException("User not logged in")
-
-    val _currentUserMutableStateFlow: MutableStateFlow<String?> = MutableStateFlow(FirebaseAuth.getInstance().currentUser?.uid)
-    override val currentUserStateFlow: StateFlow<String?> = _currentUserMutableStateFlow.asStateFlow()
-
-    init {
-        FirebaseAuth.getInstance().addAuthStateListener { auth ->
-            _currentUserMutableStateFlow.value = auth.currentUser?.uid
-        }
-    }
-
-    override val isLoggedIn: Boolean
-        get() = FirebaseAuth.getInstance().currentUser != null
-
-    override suspend fun logOut() {
-        FirebaseAuth.getInstance().signOut()
-    }
-
-    override suspend fun signInAnonymouse(): Result<Unit> {
-        return try {
-            FirebaseAuth.getInstance().signInAnonymously().await()
-            Result.success(Unit)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-    }
-
-    override suspend fun signIn(context: Context): Result<Unit> {
-        return try {
-            val googleIdOption = GetGoogleIdOption.Builder()
-                .setFilterByAuthorizedAccounts(false)
-                .setServerClientId("REPLACE_ME_STRING") //write api key heere
-                .build()
-
-            val request = GetCredentialRequest.Builder()
-                .addCredentialOption(googleIdOption)
-                .build()
-
-            val result = CredentialManager.create(context).getCredential(context,request)
-            val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(result.credential.data)
-            val firebaseCredential = GoogleAuthProvider.getCredential(googleIdTokenCredential.idToken,null)
-
-            FirebaseAuth.getInstance().signInWithCredential(firebaseCredential).await()
-            Result.success(Unit)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-    }
-}
- */
-
 // global singleton -> only one session, accessible everywhere
 object SessionManagerFacade : AuthRepository {
 
